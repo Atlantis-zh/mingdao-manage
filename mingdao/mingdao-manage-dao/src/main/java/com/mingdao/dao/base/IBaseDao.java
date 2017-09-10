f751445d6d66d2c593fb45042b133f6de8c3af6b
Atlantis-zh <13718903586@163.com>
2017-09-10 17:30:18 +0800

    删除无用文件
    Change-Id: I613e36d58a66e05c169a25ae334ed28dbbc14e8f
    Signed-off-by: Atlantis-zh <13718903586@163.com>

--- mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IBaseDao.java
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IBaseDao.java
@@ -3,7 +3,7 @@ package com.mingdao.dao.base;
 import java.util.List;
 
 public interface IBaseDao<E> {
-	public void insertVO(E entitie);
+	public int insertVO(E entitie);
 	public int insertVOs(E[] entities);
 	public int updateVO(E head);
 	public String[] nextIds(Object obj);
