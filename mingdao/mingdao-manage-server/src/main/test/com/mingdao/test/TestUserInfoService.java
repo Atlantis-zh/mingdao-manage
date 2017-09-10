b58679e16ec8f09e857e179a0d6e4f8209c4619a
Atlantis-zh <13718903586@163.com>
2017-09-10 18:00:29 +0800

    测试用例
    Change-Id: I8333c59d35034a4c5fc71f0e9bb7042b64bb0f69
    Signed-off-by: Atlantis-zh <13718903586@163.com>

---
+++ mingdao/mingdao-manage-server/src/main/test/com/mingdao/test/TestUserInfoService.java
@@ -0,0 +1,29 @@
+package com.mingdao.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mingdao.api.IUserInfoBaseServiceItf;
+import com.mingdao.domain.UserInfo;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/spring-config.xml" })
+public class TestUserInfoService {
+	
+	@Autowired
+	private IUserInfoBaseServiceItf service;
+
+	@Test
+	public void testInsert()
+	{
+		UserInfo userInfo=new UserInfo();
+		userInfo.setUserCode("0001");
+		userInfo.setUserName("libinf");
+		service.insertUserInfo(userInfo);
+	}
+}
