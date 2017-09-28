62f1f42628e6470d51786cf5b311a90d28749dd3
wushzh <18500567829@163.com>
2017-09-28 20:51:19 +0800

    微信关注列表
    Change-Id: Ifc6af8222dde386295a0f694a0bdd7372e495d4c
    Signed-off-by: wushzh <18500567829@163.com>

---
+++ mingdao/mingdao-manage-server/src/main/java/com/mingdao/server/impl/WeChetInfoBaseServiceImpl.java
@@ -0,0 +1,56 @@
+package com.mingdao.server.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.mingdao.api.IWeChetBaseService;
+import com.mingdao.common.pageUtil.PageBoundsUtil;
+import com.mingdao.common.pageUtil.Pager;
+import com.mingdao.common.utils.DateUtil;
+import com.mingdao.dao.base.IWeChetInfoDao;
+import com.mingdao.domain.WeChatInfo;
+
+/**
+*
+* <code>IWeChetInfoDao<code> <strong></strong>
+* <p>
+
+* <li>微信关注列表</li>
+* </p>
+* 
+* @since
+* @version 
+* @author wushzh
+*/
+public class WeChetInfoBaseServiceImpl implements IWeChetBaseService{
+
+	@Autowired
+	private IWeChetInfoDao dao;
+	
+	@Override
+	public WeChatInfo insertWeChatInfo(WeChatInfo weChatInfo) {
+		dao.insertVO(weChatInfo);
+		return weChatInfo;
+	}
+
+	@Override
+	public WeChatInfo updateWeChatInfo(WeChatInfo weChatInfo, Long modifyUserId) {
+		weChatInfo.setModifier(modifyUserId);
+		weChatInfo.setModifiedTime(DateUtil.getCurrentDateTime().toString());
+		dao.updateVO(weChatInfo);
+		return weChatInfo;
+	}
+
+	@Override
+	public Pager<WeChatInfo> pageQueryWeChatInfoByCondition(Map<String, Object> param) {
+		int count = dao.getCountByCondition(param);
+		PageBounds pageBounds = PageBoundsUtil.PageBoundsOrderExtend("modifiedtime.desc");
+		List<WeChatInfo> list = dao.pageQueryByCondition(param, pageBounds);
+		Pager<WeChatInfo> pages = new Pager<WeChatInfo>(count, list);
+		return pages;
+	}
+
+}
