950483309f1dcc98c8ecf5d4430cc16cca86a2b6
wushzh <18500567829@163.com>
2017-09-30 10:15:36 +0800

    预约管理服务
    Change-Id: I301cb3eb76225b1730d947ef947a882a514fdf63
    Signed-off-by: wushzh <18500567829@163.com>

---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IWeChatOrderDao.java
@@ -0,0 +1,19 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.WeChatOrder;
+
+/**
+*
+* <code>IWeChatOrderDao<code> <strong></strong>
+* <p>
+
+* <li>预约管理</li>
+* </p>
+* 
+* @since
+* @version 
+* @author wushzh
+*/
+public interface IWeChatOrderDao extends IBaseDao<WeChatOrder>{
+
+}
