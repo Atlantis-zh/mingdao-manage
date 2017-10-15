0c80e19ab90b123b866bacb6c7ec54d46ff158a6
Atlantis-zh <13718903586@163.com>
2017-10-15 12:31:42 +0800

    处理mobile端功能开发
    Change-Id: I23e75d1b68d36e6c517b6a3c3e5ff3bbc6fedcf0
    Signed-off-by: Atlantis-zh <13718903586@163.com>

---
+++ mingdao/mingdao-manage-domain/src/main/java/com/mingdao/domain/ServiceProductClass.java
@@ -0,0 +1,20 @@
+package com.mingdao.domain;
+
+/**
+ *
+ * <code>ServiceProductClass<code> <strong></strong>
+ * <p>
+ * 说明：服务产品分类
+ * <li></li>
+ * </p>
+ * 
+ * @since
+ * @version 2017年10月4日 下午1:13:30
+ * @author libinf
+ */
+
+public class ServiceProductClass extends SuperVO {
+
+	private static final long serialVersionUID = -861453484901531638L;
+
+}
---
+++ mingdao/mingdao-manage-mobile/src/main/java/com/mingdao/web/controller/SMSVerificationController.java
@@ -0,0 +1,17 @@
+package com.mingdao.web.controller;
+
+/**
+ *
+ * <code>SMSVerificationController<code>
+ * <strong></strong>
+ * <p>说明：
+ * <li></li>
+ * </p>
+ * @since 
+ * @version 2017年10月15日 下午12:18:56
+ * @author libinf
+ */
+
+public class SMSVerificationController {
+
+}
