8fe635935ef7ceb6fa1a5559e45511362fb1706b
Atlantis-zh <13718903586@163.com>
2017-10-15 15:04:06 +0800

    车辆信息基本服务
    Change-Id: I5234b908456b7083bb4e5f41ceb02c54aab8e95d
    Signed-off-by: Atlantis-zh <13718903586@163.com>

---
+++ mingdao/mingdao-manage-api/src/main/java/com/mingdao/api/ICarInfoBaseService.java
@@ -0,0 +1,20 @@
+package com.mingdao.api;
+
+import com.mingdao.domain.CarInfo;
+
+/**
+ *
+ * <code>ICarInfoBaseService<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since
+ * @version 2017年10月15日 下午2:59:03
+ * @author libinf
+ */
+
+public interface ICarInfoBaseService extends IDocBaseService<CarInfo> {
+
+}
---
+++ mingdao/mingdao-manage-server/src/main/java/com/mingdao/server/impl/CarInfoBaseServiceImpl.java
@@ -0,0 +1,58 @@
+package com.mingdao.server.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.mingdao.api.ICarInfoBaseService;
+import com.mingdao.common.pageUtil.PageBoundsUtil;
+import com.mingdao.common.pageUtil.Pager;
+import com.mingdao.common.utils.DateUtil;
+import com.mingdao.dao.base.ICarInfoDao;
+import com.mingdao.domain.CarInfo;
+
+/**
+ *
+ * <code>CarInfoBaseServiceImpl<code> <strong></strong>
+ * <p>
+ * 说明：车辆信息基本服务
+ * <li></li>
+ * </p>
+ * 
+ * @since
+ * @version 2017年10月15日 下午3:00:21
+ * @author libinf
+ */
+@Service
+public class CarInfoBaseServiceImpl implements ICarInfoBaseService {
+
+	@Autowired
+	private ICarInfoDao dao;
+
+	@Override
+	public CarInfo insert(CarInfo t) {
+		dao.insertVO(t);
+		return t;
+	}
+
+	@Override
+	public CarInfo update(CarInfo t, Long modifier) {
+		t.setModifier(modifier);
+		t.setModifiedTime(DateUtil.getCurrentDateTime().toString());
+		dao.updateVO(t);
+		return t;
+	}
+
+	@Override
+	public Pager<CarInfo> pageQueryByCondition(Map<String, Object> param) {
+		int count = dao.getCountByCondition(param);
+		PageBounds pageBounds = PageBoundsUtil.PageBoundsOrderExtend("modifiedtime.desc");
+		List<CarInfo> list = dao.pageQueryByCondition(param, pageBounds);
+		Pager<CarInfo> pages = new Pager<CarInfo>(count, list);
+		return pages;
+	}
+
+}
