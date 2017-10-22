cfa2c95183b47897fbc7f7d85f896b7fa1cfe0ad
Atlantis-zh <13718903586@163.com>
2017-10-23 00:46:37 +0800

    计量单位基本服务
    Change-Id: Ie9ed2ee060174167601c0f4011e45cbc46cb7770
    Signed-off-by: Atlantis-zh <13718903586@163.com>

---
+++ mingdao/mingdao-manage-api/src/main/java/com/mingdao/api/IMeasdocBaseService.java
@@ -0,0 +1,20 @@
+package com.mingdao.api;
+
+import com.mingdao.domain.Measdoc;
+
+/**
+ *
+ * <code>IMeasdocBaseService<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since
+ * @version 2017年10月23日 上午12:43:25
+ * @author libinf
+ */
+
+public interface IMeasdocBaseService extends IDocBaseService<Measdoc> {
+
+}
---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IMeasdocDao.java
@@ -0,0 +1,20 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.Measdoc;
+
+/**
+ *
+ * <code>IMeasdocDao<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since
+ * @version 2017年10月23日 上午12:39:43
+ * @author libinf
+ */
+
+public interface IMeasdocDao extends IBaseDao<Measdoc> {
+
+}
