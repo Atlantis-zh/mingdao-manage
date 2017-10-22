4bffb8fa265839292db2c5139d6e755b1bf2517e
Atlantis-zh <13718903586@163.com>
2017-10-22 18:55:13 +0800

    重构预约项目
    Change-Id: I70aba80bf1822e96e5cf608277e2dde3e69d8891
    Signed-off-by: Atlantis-zh <13718903586@163.com>

---
+++ mingdao/mingdao-manage-api/src/main/java/com/mingdao/api/IOrderProjectBaseService.java
@@ -0,0 +1,20 @@
+package com.mingdao.api;
+
+import com.mingdao.domain.OrderProject;
+
+/**
+ *
+ * <code>IWeChetOrderBaseService<code> <strong></strong>
+ * <p>
+ * 
+ * <li>预约管理</li>
+ * </p>
+ * 
+ * @since
+ * @version
+ * @author wushzh
+ */
+public interface IOrderProjectBaseService extends IDocBaseService<OrderProject> {
+	
+
+}
---
+++ mingdao/mingdao-manage-server/src/main/java/com/mingdao/server/impl/OrderProjectBaseServiceImpl.java
@@ -0,0 +1,64 @@
+package com.mingdao.server.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.mingdao.api.IOrderProjectBaseService;
+import com.mingdao.common.pageUtil.PageBoundsUtil;
+import com.mingdao.common.pageUtil.Pager;
+import com.mingdao.common.utils.DateUtil;
+import com.mingdao.dao.base.IOrderProjectDao;
+import com.mingdao.domain.OrderProject;
+
+/**
+ *
+ * <code>WeChetOrderInfoBaseServiceImpl<code> <strong></strong>
+ * <p>
+ * 
+ * <li>预约管理</li>
+ * </p>
+ * 
+ * @since
+ * @version
+ * @author libinf
+ */
+@Service
+public class OrderProjectBaseServiceImpl implements IOrderProjectBaseService{
+
+	@Autowired
+	private IOrderProjectDao dao;
+
+
+	@Override
+	public OrderProject insert(OrderProject t) {
+		dao.insertVO(t);
+		return t;
+	}
+
+	@Override
+	public OrderProject update(OrderProject t, Long modifier) {
+		t.setModifier(modifier);
+		t.setModifiedTime(DateUtil.getCurrentTimestamp());
+		dao.updateVO(t);
+		return t;
+	}
+
+	@Override
+	public Pager<OrderProject> pageQueryByCondition(Map<String, Object> param) {
+		int count = dao.getCountByCondition(param);
+		PageBounds pageBounds = PageBoundsUtil.PageBoundsOrderExtend("modifiedtime.desc");
+		List<OrderProject> list = dao.pageQueryByCondition(param, pageBounds);
+		Pager<OrderProject> pages = new Pager<OrderProject>(count, list);
+		return pages;
+	}
+
+	@Override
+	public OrderProject singleQryByCondtion(Map<String, Object> param) {
+		return null;
+	}
+
+}
