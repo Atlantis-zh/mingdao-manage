4827b98560c5d24990465dcc5737a8d0ccc5f2ad
Atlantis-zh <13718903586@163.com>
2017-10-23 00:26:20 +0800

    处理服务项目
    Change-Id: I342cb70b21e6776f63bbc8ff866d107e05baddf3
    Signed-off-by: Atlantis-zh <13718903586@163.com>

---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IServiceProjectDao.java
@@ -0,0 +1,20 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.ServiceProject;
+
+/**
+ *
+ * <code>IServiceProjectDao<code> <strong></strong>
+ * <p>
+ * 说明：服务项目的dao
+ * <li></li>
+ * </p>
+ * 
+ * @since
+ * @version 2017年10月23日 上午12:08:16
+ * @author libinf
+ */
+
+public interface IServiceProjectDao extends IBaseDao<ServiceProject> {
+
+}
