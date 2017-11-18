86b62e9eb6e9691fa5d54bbadacef268c130759e
libin <13718903586@163.com>
2017-11-19 01:50:17 +0800

    订单管理服务开发

---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IOrderFormDao.java
@@ -0,0 +1,20 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.OrderForm;
+
+/**
+ *
+ * <code>IOrderFormDao<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月19日 上午1:39:45
+ * @author libin
+ */
+
+public interface IOrderFormDao extends IBaseDao<OrderForm> {
+
+}
---
+++ mingdao/mingdao-manage-server/src/main/java/com/mingdao/server/impl/OrderFormBaseServiceImpl.java
@@ -0,0 +1,67 @@
+package com.mingdao.server.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.mingdao.api.IOrderFormBaseService;
+import com.mingdao.common.pageUtil.PageBoundsUtil;
+import com.mingdao.common.pageUtil.Pager;
+import com.mingdao.dao.base.IOrderFormDao;
+import com.mingdao.domain.OrderForm;
+
+/**
+ *
+ * <code>OrderFormBaseServiceImpl<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月19日 上午1:48:26
+ * @author libin
+ */
+@Service
+public class OrderFormBaseServiceImpl implements IOrderFormBaseService {
+  @Autowired
+  private IOrderFormDao dao;
+
+  @Override
+  public OrderForm insert(OrderForm t) {
+    dao.insertVO(t);
+    return t;
+  }
+
+  @Override
+  public OrderForm update(OrderForm t) {
+    dao.updateVO(t);
+    return t;
+  }
+
+  @Override
+  public Pager<OrderForm> pageQueryByCondition(Map<String, Object> param) {
+    int count = dao.getCountByCondition(param);
+    PageBounds pageBounds = PageBoundsUtil.PageBoundsOrderExtend("modifiedtime.desc");
+    List<OrderForm> list = dao.pageQueryByCondition(param, pageBounds);
+    Pager<OrderForm> pages = new Pager<OrderForm>(count, list);
+    return pages;
+  }
+
+  @Override
+  public OrderForm singleQryByCondtion(Map<String, Object> param) {
+    return null;
+  }
+
+  @Override
+  public List<OrderForm> qryAllDoces(Map<String, Object> param) {
+    return null;
+  }
+
+  @Override
+  public void deleteDocById(Long id) {}
+
+}
