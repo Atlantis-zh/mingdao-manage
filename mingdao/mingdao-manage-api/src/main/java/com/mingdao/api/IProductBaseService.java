d526fa9c897046a66775d024e2453021ed51380a
libin <13718903586@163.com>
2017-11-18 22:06:45 +0800

    处理商品信息的服务

---
+++ mingdao/mingdao-manage-api/src/main/java/com/mingdao/api/IProductBaseService.java
@@ -0,0 +1,20 @@
+package com.mingdao.api;
+
+import com.mingdao.domain.Product;
+
+/**
+ *
+ * <code>IProductBaseService<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月18日 下午10:02:34
+ * @author libin
+ */
+
+public interface IProductBaseService extends IDocBaseService<Product> {
+
+}
---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IProductDao.java
@@ -0,0 +1,20 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.Product;
+
+/**
+ *
+ * <code>IProductDao<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月18日 下午9:24:37
+ * @author libin
+ */
+
+public interface IProductDao extends IBaseDao<Product> {
+
+}
