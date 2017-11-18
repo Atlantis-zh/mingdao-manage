d2c00f588fb192e64e2d502b7a76a6e35ef356a1
libin <13718903586@163.com>
2017-11-19 00:46:21 +0800

    会员套餐项目明细服务

---
+++ mingdao/mingdao-manage-api/src/main/java/com/mingdao/api/IMemShipPackageProjectDetailBaseService.java
@@ -0,0 +1,21 @@
+package com.mingdao.api;
+
+import com.mingdao.domain.MemShipPackageProjectDetail;
+
+/**
+ *
+ * <code>IMemShipPackageProjectDetailBaseService<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月19日 上午12:43:55
+ * @author libin
+ */
+
+public interface IMemShipPackageProjectDetailBaseService
+    extends IDocBaseService<MemShipPackageProjectDetail> {
+
+}
---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IMemShipPackageProjectDetailDao.java
@@ -0,0 +1,20 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.MemShipPackageProjectDetail;
+
+/**
+ *
+ * <code>IMemShipPackageItemDetailDao<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月19日 上午12:34:56
+ * @author libin
+ */
+
+public interface IMemShipPackageProjectDetailDao extends IBaseDao<MemShipPackageProjectDetail> {
+
+}
