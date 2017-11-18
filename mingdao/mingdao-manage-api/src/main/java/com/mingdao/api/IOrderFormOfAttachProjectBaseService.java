48c66bc2ca1169d98340ed46ae864daddc3f8120
libin <13718903586@163.com>
2017-11-19 02:34:51 +0800

    附加项目订单

---
+++ mingdao/mingdao-manage-api/src/main/java/com/mingdao/api/IOrderFormOfAttachProjectBaseService.java
@@ -0,0 +1,21 @@
+package com.mingdao.api;
+
+import com.mingdao.domain.OrderFormOfAttachProject;
+
+/**
+ *
+ * <code>IOrderFormOfAttachProjectBaseService<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月19日 上午2:32:25
+ * @author libin
+ */
+
+public interface IOrderFormOfAttachProjectBaseService
+    extends IDocBaseService<OrderFormOfAttachProject> {
+
+}
---
+++ mingdao/mingdao-manage-dao/src/main/java/com/mingdao/dao/base/IOrderFormOfAttachProjectDao.java
@@ -0,0 +1,20 @@
+package com.mingdao.dao.base;
+
+import com.mingdao.domain.OrderFormOfAttachProject;
+
+/**
+ *
+ * <code>IOrderFormOfAttachProjectDao<code> <strong></strong>
+ * <p>
+ * 说明：
+ * <li></li>
+ * </p>
+ * 
+ * @since NC6.5
+ * @version 2017年11月19日 上午2:29:25
+ * @author libin
+ */
+
+public interface IOrderFormOfAttachProjectDao extends IBaseDao<OrderFormOfAttachProject> {
+
+}
